e4bcdfb2cc29ae42b8737421d8f85a5437c9ad51
Abdullahi <muxsinxareed6@gmail.com>
2024-02-11 20:09:36 +0100

    Enable audio playback functionality on play button click

---
+++ data/luhaidanAudios.js
@@ -0,0 +1,598 @@
+const luhaidanAudios = [
+    {
+        "chapterNumber": "001",
+        "chapterName": "al-fatihah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/001.mp3"
+    },
+    {
+        "chapterNumber": "002",
+        "chapterName": "al-baqarah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/002.mp3"
+    },
+    {
+        "chapterNumber": "003",
+        "chapterName": "al-imran",
+        "audioLink": "https://server8.mp3quran.net/lhdan/003.mp3"
+    },
+    {
+        "chapterNumber": "004",
+        "chapterName": "an-nisa'",
+        "audioLink": "https://server8.mp3quran.net/lhdan/004.mp3"
+    },
+    {
+        "chapterNumber": "005",
+        "chapterName": "al-ma'idah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/005.mp3"
+    },
+    {
+        "chapterNumber": "006",
+        "chapterName": "al-an'am",
+        "audioLink": "https://server8.mp3quran.net/lhdan/006.mp3"
+    },
+    {
+        "chapterNumber": "007",
+        "chapterName": "al-a'raf",
+        "audioLink": "https://server8.mp3quran.net/lhdan/007.mp3"
+    },
+    {
+        "chapterNumber": "008",
+        "chapterName": "al-anfal",
+        "audioLink": "https://server8.mp3quran.net/lhdan/008.mp3"
+    },
+    {
+        "chapterNumber": "009",
+        "chapterName": "at-taubah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/009.mp3"
+    },
+    {
+        "chapterNumber": "010",
+        "chapterName": "yunus",
+        "audioLink": "https://server8.mp3quran.net/lhdan/010.mp3"
+    },
+    {
+        "chapterNumber": "011",
+        "chapterName": "hood",
+        "audioLink": "https://server8.mp3quran.net/lhdan/011.mp3"
+    },
+    {
+        "chapterNumber": "012",
+        "chapterName": "yusuf",
+        "audioLink": "https://server8.mp3quran.net/lhdan/012.mp3"
+    },
+    {
+        "chapterNumber": "013",
+        "chapterName": "ar-ra'd",
+        "audioLink": "https://server8.mp3quran.net/lhdan/013.mp3"
+    },
+    {
+        "chapterNumber": "014",
+        "chapterName": "ibrahim",
+        "audioLink": "https://server8.mp3quran.net/lhdan/014.mp3"
+    },
+    {
+        "chapterNumber": "015",
+        "chapterName": "al-hijr",
+        "audioLink": "https://server8.mp3quran.net/lhdan/015.mp3"
+    },
+    {
+        "chapterNumber": "016",
+        "chapterName": "an-nahl",
+        "audioLink": "https://server8.mp3quran.net/lhdan/016.mp3"
+    },
+    {
+        "chapterNumber": "017",
+        "chapterName": "al-isra",
+        "audioLink": "https://server8.mp3quran.net/lhdan/017.mp3"
+    },
+    {
+        "chapterNumber": "018",
+        "chapterName": "al-kahf",
+        "audioLink": "https://server8.mp3quran.net/lhdan/018.mp3"
+    },
+    {
+        "chapterNumber": "019",
+        "chapterName": "maryam",
+        "audioLink": "https://server8.mp3quran.net/lhdan/019.mp3"
+    },
+    {
+        "chapterNumber": "020",
+        "chapterName": "ta­ha",
+        "audioLink": "https://server8.mp3quran.net/lhdan/020.mp3"
+    },
+    {
+        "chapterNumber": "021",
+        "chapterName": "al-anbiya'",
+        "audioLink": "https://server8.mp3quran.net/lhdan/021.mp3"
+    },
+    {
+        "chapterNumber": "022",
+        "chapterName": "al-hajj",
+        "audioLink": "https://server8.mp3quran.net/lhdan/022.mp3"
+    },
+    {
+        "chapterNumber": "023",
+        "chapterName": "al-mu'minun",
+        "audioLink": "https://server8.mp3quran.net/lhdan/023.mp3"
+    },
+    {
+        "chapterNumber": "024",
+        "chapterName": "an-nur",
+        "audioLink": "https://server8.mp3quran.net/lhdan/024.mp3"
+    },
+    {
+        "chapterNumber": "025",
+        "chapterName": "al-furqan",
+        "audioLink": "https://server8.mp3quran.net/lhdan/025.mp3"
+    },
+    {
+        "chapterNumber": "026",
+        "chapterName": "ash-shu'ara'",
+        "audioLink": "https://server8.mp3quran.net/lhdan/026.mp3"
+    },
+    {
+        "chapterNumber": "027",
+        "chapterName": "an-naml",
+        "audioLink": "https://server8.mp3quran.net/lhdan/027.mp3"
+    },
+    {
+        "chapterNumber": "028",
+        "chapterName": "al-qasas",
+        "audioLink": "https://server8.mp3quran.net/lhdan/028.mp3"
+    },
+    {
+        "chapterNumber": "029",
+        "chapterName": "al-'ankabut",
+        "audioLink": "https://server8.mp3quran.net/lhdan/029.mp3"
+    },
+    {
+        "chapterNumber": "030",
+        "chapterName": "ar­room",
+        "audioLink": "https://server8.mp3quran.net/lhdan/030.mp3"
+    },
+    {
+        "chapterNumber": "031",
+        "chapterName": "luqman",
+        "audioLink": "https://server8.mp3quran.net/lhdan/031.mp3"
+    },
+    {
+        "chapterNumber": "032",
+        "chapterName": "as­sajdah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/032.mp3"
+    },
+    {
+        "chapterNumber": "033",
+        "chapterName": "al­ahzab",
+        "audioLink": "https://server8.mp3quran.net/lhdan/033.mp3"
+    },
+    {
+        "chapterNumber": "034",
+        "chapterName": "saba'",
+        "audioLink": "https://server8.mp3quran.net/lhdan/034.mp3"
+    },
+    {
+        "chapterNumber": "035",
+        "chapterName": "fatir",
+        "audioLink": "https://server8.mp3quran.net/lhdan/035.mp3"
+    },
+    {
+        "chapterNumber": "036",
+        "chapterName": "ya­sin",
+        "audioLink": "https://server8.mp3quran.net/lhdan/036.mp3"
+    },
+    {
+        "chapterNumber": "037",
+        "chapterName": "as-saffat",
+        "audioLink": "https://server8.mp3quran.net/lhdan/037.mp3"
+    },
+    {
+        "chapterNumber": "038",
+        "chapterName": "sad",
+        "audioLink": "https://server8.mp3quran.net/lhdan/038.mp3"
+    },
+    {
+        "chapterNumber": "039",
+        "chapterName": "az-zumar",
+        "audioLink": "https://server8.mp3quran.net/lhdan/039.mp3"
+    },
+    {
+        "chapterNumber": "040",
+        "chapterName": "ghafir",
+        "audioLink": "https://server8.mp3quran.net/lhdan/040.mp3"
+    },
+    {
+        "chapterNumber": "041",
+        "chapterName": "fussilat",
+        "audioLink": "https://server8.mp3quran.net/lhdan/041.mp3"
+    },
+    {
+        "chapterNumber": "042",
+        "chapterName": "ash-shura",
+        "audioLink": "https://server8.mp3quran.net/lhdan/042.mp3"
+    },
+    {
+        "chapterNumber": "043",
+        "chapterName": "az-zukhruf",
+        "audioLink": "https://server8.mp3quran.net/lhdan/043.mp3"
+    },
+    {
+        "chapterNumber": "044",
+        "chapterName": "ad-dukhan",
+        "audioLink": "https://server8.mp3quran.net/lhdan/044.mp3"
+    },
+    {
+        "chapterNumber": "045",
+        "chapterName": "al-jathiya",
+        "audioLink": "https://server8.mp3quran.net/lhdan/045.mp3"
+    },
+    {
+        "chapterNumber": "046",
+        "chapterName": "al-ahqaf",
+        "audioLink": "https://server8.mp3quran.net/lhdan/046.mp3"
+    },
+    {
+        "chapterNumber": "047",
+        "chapterName": "muhammad",
+        "audioLink": "https://server8.mp3quran.net/lhdan/047.mp3"
+    },
+    {
+        "chapterNumber": "048",
+        "chapterName": "al-fath",
+        "audioLink": "https://server8.mp3quran.net/lhdan/048.mp3"
+    },
+    {
+        "chapterNumber": "049",
+        "chapterName": "al-hujurat",
+        "audioLink": "https://server8.mp3quran.net/lhdan/049.mp3"
+    },
+    {
+        "chapterNumber": "050",
+        "chapterName": "qaf",
+        "audioLink": "https://server8.mp3quran.net/lhdan/050.mp3"
+    },
+    {
+        "chapterNumber": "051",
+        "chapterName": "az-zariyat",
+        "audioLink": "https://server8.mp3quran.net/lhdan/051.mp3"
+    },
+    {
+        "chapterNumber": "052",
+        "chapterName": "at-tur",
+        "audioLink": "https://server8.mp3quran.net/lhdan/052.mp3"
+    },
+    {
+        "chapterNumber": "053",
+        "chapterName": "an-najm",
+        "audioLink": "https://server8.mp3quran.net/lhdan/053.mp3"
+    },
+    {
+        "chapterNumber": "054",
+        "chapterName": "al-qamar",
+        "audioLink": "https://server8.mp3quran.net/lhdan/054.mp3"
+    },
+    {
+        "chapterNumber": "055",
+        "chapterName": "ar-rahman",
+        "audioLink": "https://server8.mp3quran.net/lhdan/055.mp3"
+    },
+    {
+        "chapterNumber": "056",
+        "chapterName": "al-waqi'ah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/056.mp3"
+    },
+    {
+        "chapterNumber": "057",
+        "chapterName": "al-hadid",
+        "audioLink": "https://server8.mp3quran.net/lhdan/057.mp3"
+    },
+    {
+        "chapterNumber": "058",
+        "chapterName": "al-mujadilah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/058.mp3"
+    },
+    {
+        "chapterNumber": "059",
+        "chapterName": "al-hashr",
+        "audioLink": "https://server8.mp3quran.net/lhdan/059.mp3"
+    },
+    {
+        "chapterNumber": "060",
+        "chapterName": "al-mumtahinah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/060.mp3"
+    },
+    {
+        "chapterNumber": "061",
+        "chapterName": "as-saff",
+        "audioLink": "https://server8.mp3quran.net/lhdan/061.mp3"
+    },
+    {
+        "chapterNumber": "062",
+        "chapterName": "al-jumu'ah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/062.mp3"
+    },
+    {
+        "chapterNumber": "063",
+        "chapterName": "al-munafiqun",
+        "audioLink": "https://server8.mp3quran.net/lhdan/063.mp3"
+    },
+    {
+        "chapterNumber": "064",
+        "chapterName": "at-taghabun",
+        "audioLink": "https://server8.mp3quran.net/lhdan/064.mp3"
+    },
+    {
+        "chapterNumber": "065",
+        "chapterName": "at-talaq",
+        "audioLink": "https://server8.mp3quran.net/lhdan/065.mp3"
+    },
+    {
+        "chapterNumber": "066",
+        "chapterName": "at-tahrim",
+        "audioLink": "https://server8.mp3quran.net/lhdan/066.mp3"
+    },
+    {
+        "chapterNumber": "067",
+        "chapterName": "al-mulk",
+        "audioLink": "https://server8.mp3quran.net/lhdan/067.mp3"
+    },
+    {
+        "chapterNumber": "068",
+        "chapterName": "al-qalam",
+        "audioLink": "https://server8.mp3quran.net/lhdan/068.mp3"
+    },
+    {
+        "chapterNumber": "069",
+        "chapterName": "al-haqqah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/069.mp3"
+    },
+    {
+        "chapterNumber": "070",
+        "chapterName": "al-ma'arij",
+        "audioLink": "https://server8.mp3quran.net/lhdan/070.mp3"
+    },
+    {
+        "chapterNumber": "071",
+        "chapterName": "nooh",
+        "audioLink": "https://server8.mp3quran.net/lhdan/071.mp3"
+    },
+    {
+        "chapterNumber": "072",
+        "chapterName": "al-jinn",
+        "audioLink": "https://server8.mp3quran.net/lhdan/072.mp3"
+    },
+    {
+        "chapterNumber": "073",
+        "chapterName": "al-muzzammil",
+        "audioLink": "https://server8.mp3quran.net/lhdan/073.mp3"
+    },
+    {
+        "chapterNumber": "074",
+        "chapterName": "al-muddaththir",
+        "audioLink": "https://server8.mp3quran.net/lhdan/074.mp3"
+    },
+    {
+        "chapterNumber": "075",
+        "chapterName": "al-qiyamah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/075.mp3"
+    },
+    {
+        "chapterNumber": "076",
+        "chapterName": "al-insan",
+        "audioLink": "https://server8.mp3quran.net/lhdan/076.mp3"
+    },
+    {
+        "chapterNumber": "077",
+        "chapterName": "al-mursalat",
+        "audioLink": "https://server8.mp3quran.net/lhdan/077.mp3"
+    },
+    {
+        "chapterNumber": "078",
+        "chapterName": "an-naba'",
+        "audioLink": "https://server8.mp3quran.net/lhdan/078.mp3"
+    },
+    {
+        "chapterNumber": "079",
+        "chapterName": "an-nazi'at",
+        "audioLink": "https://server8.mp3quran.net/lhdan/079.mp3"
+    },
+    {
+        "chapterNumber": "080",
+        "chapterName": "abasa",
+        "audioLink": "https://server8.mp3quran.net/lhdan/080.mp3"
+    },
+    {
+        "chapterNumber": "081",
+        "chapterName": "at-takwir",
+        "audioLink": "https://server8.mp3quran.net/lhdan/081.mp3"
+    },
+    {
+        "chapterNumber": "082",
+        "chapterName": "al-infitar",
+        "audioLink": "https://server8.mp3quran.net/lhdan/082.mp3"
+    },
+    {
+        "chapterNumber": "083",
+        "chapterName": "al-mutaffifin",
+        "audioLink": "https://server8.mp3quran.net/lhdan/083.mp3"
+    },
+    {
+        "chapterNumber": "084",
+        "chapterName": "al-inshiqaq",
+        "audioLink": "https://server8.mp3quran.net/lhdan/084.mp3"
+    },
+    {
+        "chapterNumber": "085",
+        "chapterName": "al-buruj",
+        "audioLink": "https://server8.mp3quran.net/lhdan/085.mp3"
+    },
+    {
+        "chapterNumber": "086",
+        "chapterName": "at-tariq",
+        "audioLink": "https://server8.mp3quran.net/lhdan/086.mp3"
+    },
+    {
+        "chapterNumber": "087",
+        "chapterName": "al-a'la",
+        "audioLink": "https://server8.mp3quran.net/lhdan/087.mp3"
+    },
+    {
+        "chapterNumber": "088",
+        "chapterName": "al-ghashiyah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/088.mp3"
+    },
+    {
+        "chapterNumber": "089",
+        "chapterName": "al-fajr",
+        "audioLink": "https://server8.mp3quran.net/lhdan/089.mp3"
+    },
+    {
+        "chapterNumber": "090",
+        "chapterName": "al-balad",
+        "audioLink": "https://server8.mp3quran.net/lhdan/090.mp3"
+    },
+    {
+        "chapterNumber": "091",
+        "chapterName": "ash-shams",
+        "audioLink": "https://server8.mp3quran.net/lhdan/091.mp3"
+    },
+    {
+        "chapterNumber": "092",
+        "chapterName": "al-lail",
+        "audioLink": "https://server8.mp3quran.net/lhdan/092.mp3"
+    },
+    {
+        "chapterNumber": "093",
+        "chapterName": "ad-duha",
+        "audioLink": "https://server8.mp3quran.net/lhdan/093.mp3"
+    },
+    {
+        "chapterNumber": "094",
+        "chapterName": "ash-sharh",
+        "audioLink": "https://server8.mp3quran.net/lhdan/094.mp3"
+    },
+    {
+        "chapterNumber": "095",
+        "chapterName": "at-tin",
+        "audioLink": "https://server8.mp3quran.net/lhdan/095.mp3"
+    },
+    {
+        "chapterNumber": "096",
+        "chapterName": "al-'alaq",
+        "audioLink": "https://server8.mp3quran.net/lhdan/096.mp3"
+    },
+    {
+        "chapterNumber": "097",
+        "chapterName": "al-qadr",
+        "audioLink": "https://server8.mp3quran.net/lhdan/097.mp3"
+    },
+    {
+        "chapterNumber": "098",
+        "chapterName": "al-baiyinah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/098.mp3"
+    },
+    {
+        "chapterNumber": "099",
+        "chapterName": "az-zalzalah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/099.mp3"
+    },
+    {
+        "chapterNumber": "100",
+        "chapterName": "al-'adiyat",
+        "audioLink": "https://server8.mp3quran.net/lhdan/100.mp3"
+    },
+    {
+        "chapterNumber": "101",
+        "chapterName": "al-qari'ah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/101.mp3"
+    },
+    {
+        "chapterNumber": "102",
+        "chapterName": "at-takathur",
+        "audioLink": "https://server8.mp3quran.net/lhdan/102.mp3"
+    },
+    {
+        "chapterNumber": "103",
+        "chapterName": "al-'asr",
+        "audioLink": "https://server8.mp3quran.net/lhdan/103.mp3"
+    },
+    {
+        "chapterNumber": "104",
+        "chapterName": "al-humazah",
+        "audioLink": "https://server8.mp3quran.net/lhdan/104.mp3"
+    },
+    {
+        "chapterNumber": "105",
+        "chapterName": "al-fil",
+        "audioLink": "https://server8.mp3quran.net/lhdan/105.mp3"
+    },
+    {
+        "chapterNumber": "106",
+        "chapterName": "quraish",
+        "audioLink": "https://server8.mp3quran.net/lhdan/106.mp3"
+    },
+    {
+        "chapterNumber": "107",
+        "chapterName": "al-ma'un",
+        "audioLink": "https://server8.mp3quran.net/lhdan/107.mp3"
+    },
+    {
+        "chapterNumber": "108",
+        "chapterName": "al-kauthar",
+        "audioLink": "https://server8.mp3quran.net/lhdan/108.mp3"
+    },
+    {
+        "chapterNumber": "109",
+        "chapterName": "al-kafirun",
+        "audioLink": "https://server8.mp3quran.net/lhdan/109.mp3"
+    },
+    {
+        "chapterNumber": "110",
+        "chapterName": "an-nasr",
+        "audioLink": "https://server8.mp3quran.net/lhdan/110.mp3"
+    },
+    {
+        "chapterNumber": "111",
+        "chapterName": "al-masad",
+        "audioLink": "https://server8.mp3quran.net/lhdan/111.mp3"
+    },
+    {
+        "chapterNumber": "112",
+        "chapterName": "al-ikhlas",
+        "audioLink": "https://server8.mp3quran.net/lhdan/112.mp3"
+    },
+    {
+        "chapterNumber": "113",
+        "chapterName": "al-falaq",
+        "audioLink": "https://server8.mp3quran.net/lhdan/113.mp3"
+    },
+    {
+        "chapterNumber": "114",
+        "chapterName": "an-nas",
+        "audioLink": "https://server8.mp3quran.net/lhdan/114.mp3"
+    },
+]
+
+const content = document.querySelector('.content');
+const currentTrack = document.querySelector('.currentTruck');
+
+luhaidanAudios.forEach((surah, index) => {
+    const chapterSection = document.createElement('div');
+    chapterSection.classList.add('chapterSection');
+    chapterSection.innerHTML = `
+        <div class="info">
+            <h2>Number: ${surah.chapterNumber}</h2>
+            <div>Name: ${surah.chapterName}</div>
+        </div>
+        <img class="audioIcon" src="/main/images/play(1).svg" alt="Icon for playing the audio">
+    `;
+
+    const audioPlayer = chapterSection.querySelector('.audioIcon');
+    const currentTrack = document.querySelector('.playingSection > audio');
+    const playingSection  = document.querySelector('.playingSection');
+    audioPlayer.addEventListener('click', function () {
+        currentTrack.src = surah.audioLink;
+        playingSection.style.display = "block"
+        currentTrack.play();
+    });
+
+    content.appendChild(chapterSection);
+});
